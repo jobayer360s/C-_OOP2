0884220884acb60d5aab22865c6a28c97927c132
Jobayer <jobayer360s@gmail.com>
2024-11-13 13:10:47 +0600

    As is , class object file

---
+++ Lab/7.As_is/7.As_is/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7.As_is
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            object a = 10;
+            bool b = a is string; // is operator is used for type cheack
+            Console.WriteLine(b);
+
+            object c = "10"; // if object = 10, then output will show null as it is not string according to next line 
+            string d = c as string;
+            Console.WriteLine(d);
+           
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lab/8.methodClassObject/8.methodClassObject/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8.methodClassObject
+{
+    class Human
+
+    {
+        private int id;
+        public int sabbirHossin(int val) {
+            val+= 10 ;
+            Console.WriteLine("Sabbir Hossain Called {0}", val);
+             return val;
+        
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Human h1 = new Human();
+            Console.WriteLine(h1.sabbirHossin(10));
+            
+          // to access static method we don't need any Object, just use classname.MethodName()
+            Console.ReadKey();
+        }
+    }
+}
