afa9ff3f22a26035b534fdf2eca3c66ac9d270d7
Jobayer <jobayer360s@gmail.com>
2024-11-18 13:10:14 +0600

    Adding method overloading and Inheritance

---
+++ Lab/12.methodOverloading_chk/12.Inheritance/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12.MethodOverLoading
+
+{
+    class Additon
+    {
+        public int x;
+        public int y;
+        public int z;
+
+        public int sum(int x, int y)
+        {
+            int summation;
+            return summation = x + y;
+        }
+        public int sum(int x, int y, int z) {
+            int summation;
+            return summation = x+y+z;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Additon additon = new Additon();
+            Console.WriteLine(additon.sum(1,2));
+            Console.WriteLine(additon.sum(1, 2,3));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab/13.Inheritance/13.Inheritance/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13.Inheritance
+{
+    class Person
+    {
+        private string name;
+        private int age;
+
+        public Person() { }
+
+        public Person(String name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        //set get different rule
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        public void Display()
+        {
+            Console.WriteLine(Name + " " + age);
+        }
+    }
+    class Employee : Person
+    {
+
+        private int salary;
+        public Employee() { }
+        public Employee(string name, int salary)
+        {
+            this.salary = salary;
+            this.Name = name;
+
+        }
+
+        public int Salary
+        {
+            get { return salary; }
+
+
+        }
+
+        public void Display1() {
+            base.Display();
+            Console.Write("Salary  "+Salary);
+        }
+
+    }
+
+    class Student
+    {
+
+
+    }
+    //Its a hierarcycal inheritance,
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person("Dr younus" , 81);
+            p1.Display();
+            Employee p2 = new Employee("Hasina ", 100);
+            p2.Display1();
+            Console.ReadKey();
+        }
+    }
+}
