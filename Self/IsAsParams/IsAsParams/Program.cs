2f773ddacc1e9aaaf8bc4af5afbf672fdafa67a5
Jobayer <jobayer360s@gmail.com>
2024-11-24 22:23:56 +0600

    Adding is as params , ref out files in Self Folder while practice session

---
+++ Self/IsAsParams/IsAsParams/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsAsParams
+{
+    public class p { }
+    public class p1 : p { }
+
+    public class p2 { }
+
+
+
+    internal class Program
+    {
+
+        // Use of Params 
+
+        public static int Add(params int[] numbers)
+        {
+            int total = 0;
+
+            //foreach (int value in numbers)
+            //{
+            //    total += value; // Add each number to the total
+            //}
+
+            for (int i = 0; i < numbers.Length; i++) 
+            {
+             total = total + numbers[i];
+            
+            }
+
+            return total; // Return the total sum
+        }
+
+        static void Main(string[] args)
+        {
+            int a = 100;
+            bool b = a is int;
+            Console.WriteLine(b);
+            Console.WriteLine(a is string);
+
+           p o1 = new p();
+            p1 o2 = new p1();
+
+            Console.WriteLine(o1 is p);
+            Console.WriteLine(o1 is Object);
+            Console.WriteLine(o2 is p);
+            Console.WriteLine(o2 is p1);
+
+
+            Console.WriteLine("As Operator ");
+            Object c = "Hello ";
+            string d = c as String;
+            Console.WriteLine(d);
+
+            Object[] o = new Object[5];
+            o[0] = new p();
+            o[1] = new p();
+            o[2] = new p();
+            o[3] = "I love you too ";
+            o[4] = "I love crow ";
+
+            Console.WriteLine();
+            Console.WriteLine();
+           
+            for (int i = 0; i < o.Length; i++)
+            {
+                string x = o[i] as string;
+                if (x == null) {
+                    Console.WriteLine("No string , Null ");
+                }
+                else
+                {
+                    Console.WriteLine(x);
+                }
+
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Params ");
+
+            int[] numbers = new int[5] { 10, 20 , 30 ,40 ,  50};
+            int summation = Add(numbers);
+            Console.WriteLine("The summation is {0}",summation);
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Self/refOutEnam/refOutEnam/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace refOutEnam
+{
+    class Program
+    {
+        //ref , ref can modify values
+        public static void ModifyValue(ref int x) {
+
+            x *= 5;
+        
+        }
+
+        public static void initialization(out int x)
+        {
+            x = 10;
+        }
+
+        // enum
+
+        enum Level
+        {
+            Low, Medium, High
+        }
+
+        enum Months
+        {
+            January, 
+            February , 
+            March ,
+            April ,
+            May ,
+            June, 
+            July,
+            Augast ,
+            September , 
+            October ,
+            November , 
+            December
+        }
+
+        static void Main(string[] args)
+        {
+            //ref ,and out are used for pass value by aurguments
+            //ref can modify values
+            int a = 20;
+            Console.WriteLine("The value of 'a' before modify {0}",a);
+            ModifyValue(ref a);
+            Console.WriteLine("The value of 'a' after modify {0}", a);
+
+            int b;
+            //Console.WriteLine("The value of 'b' before initalization {0}", b); // its not possible as it is not initialized
+            initialization(out b);
+            Console.WriteLine("The value of 'b' after initalization {0}", b);
+
+            // what happened after passing initalized value, Lets check it.
+
+            int c = 20;
+            initialization(out c);
+            Console.WriteLine(c);
+
+            Console.WriteLine();
+            Console.WriteLine("Enum out puts");
+           Level l1 = Level.Medium;
+            Console.WriteLine(l1);
+
+            Console.WriteLine("Enum 2");
+            int months = (int)Months.June;
+            Console.WriteLine(months);
+
+            Console.ReadKey();
+        }
+    }
+}
